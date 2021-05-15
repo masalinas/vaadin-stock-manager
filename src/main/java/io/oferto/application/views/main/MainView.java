8c81d7ccef304b1475a48bb4c4ece76c90036990
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2021-05-15 11:20:21 +0200

    add security context

--- src/main/java/io/oferto/application/views/main/MainView.java
+++ src/main/java/io/oferto/application/views/main/MainView.java
@@ -126,8 +126,7 @@ public class MainView extends AppLayout {
     	        	
     	});
 
-    	contextMenu.addItem("Logout", e -> {    		
-    		//contextMenu.getUI().ifPresent(ui -> ui.getPage().open("logout"));
+    	contextMenu.addItem("Logout", e -> {
     		contextMenu.getUI().ifPresent(ui -> ui.getPage().setLocation("/logout"));
     	});
     	
