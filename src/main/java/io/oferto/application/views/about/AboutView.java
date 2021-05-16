1fcb6af5c2a83b20144e96236b1abef58e042391
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2021-05-16 18:40:16 +0200

    last update

--- src/main/java/io/oferto/application/views/about/AboutView.java
+++ src/main/java/io/oferto/application/views/about/AboutView.java
@@ -1,8 +1,9 @@
 package io.oferto.application.views.about;
 
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 
 import io.oferto.application.views.main.MainView;
 
@@ -12,11 +13,18 @@ import com.vaadin.flow.component.dependency.CssImport;
 @Route(value = "about", layout = MainView.class)
 @PageTitle("About")
 @CssImport("./views/about/about-view.css")
-public class AboutView extends Div {
+public class AboutView extends HorizontalLayout {
 
     public AboutView() {
         addClassName("about-view");
-        add(new Text("Content placeholder"));
+        
+        setSizeFull();
+        setPadding(true);
+        
+        this.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        this.setAlignItems(FlexComponent.Alignment.CENTER);
+        
+        add(new Text("Hello Stock Manager!"));
     }
 
 }
